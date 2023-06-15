e340f7174894ecc034eb7a14c41ac7cd767b16fd
Ujjwal P <ujjwal.t440p@gmail.com>
2023-06-15 11:36:56 +0000

    Added network moudule2

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
-module "network" {
+module "vnet" {
   source  = "app.terraform.io/ujjwal-terraform/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "vnet"
+  #resource_group_name = "vnet"
 }
